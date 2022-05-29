930a564adbd4321985dbef1e8414925353af370d
Bhavesh Ramburn <nitrogen@gmail.com>
2022-05-29 15:45:16 +0100

    entity: add employee entity https://trello.com/c/t82S5cBk

---
+++ src/main/java/co/saltpay/Domain/Entity/Employees/Employee.java
@@ -0,0 +1,10 @@
+package co.saltpay.Domain.Entity.Employees;
+
+import java.util.Date;
+
+public class Employee {
+    public int Id;
+    public String first_name;
+    public String last_name;
+    public Date dateOfBirth;
+}
