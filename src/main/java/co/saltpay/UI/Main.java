27a63c5637735f5daa73738b0e20340cd977ee73
Bhavesh Ramburn <nitrogen@gmail.com>
2022-05-29 23:48:23 +0100

    test: Created test for EmployeeRepository

--- src/main/java/co/saltpay/UI/Main.java
+++ src/main/java/co/saltpay/UI/Main.java
@@ -1,7 +1,14 @@
 package co.saltpay.UI;
 
+import co.saltpay.Infrastructure.Repository.Driver.JsonDriver;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ParseException, java.text.ParseException {
         System.out.println("Hello world!");
+        JsonDriver jd = new JsonDriver();
+        jd.readFile();
     }
 }
--- src/test/java/co/saltpay/Infrastructure/Repository/EmployeeRepositoryTest.java
+++ src/test/java/co/saltpay/Infrastructure/Repository/EmployeeRepositoryTest.java
@@ -1,13 +1,34 @@
 package co.saltpay.Infrastructure.Repository;
 
+import co.saltpay.Domain.Entity.Employees.Employee;
+import co.saltpay.Infrastructure.Common.Interface.IDriverData;
+import co.saltpay.Infrastructure.Repository.Model.EmployeeModel;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmployeeRepositoryTest {
 
     @Test
-    void getTodayBirthdays() {
+    void checkMoqDa() {
         // provide moq data
+
+    }
+
+
+}
+class MoqDriver implements IDriverData{
+
+    @Override
+    public Employee[] readFile() {
+        List<Employee> employees = null;
+        employees.add(new EmployeeModel("Kaps","Ramburn","1988/02/29"));
+        return null;
+    }
+
+    private void makeDate(int year, int month, int day){
+
     }
 }
