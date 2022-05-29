7b8bd3f6601226351115e9cd5bb4b7a47d2514c1
Bhavesh Ramburn <nitrogen@gmail.com>
2022-05-29 15:41:12 +0100

    test: added junit and tested it works.

--- src/test/java/co/saltpay/unitTest/MainTest.java
+++ src/test/java/co/saltpay/unitTest/MainTest.java
@@ -1,4 +1,13 @@
 package co.saltpay.unitTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class MainTest {
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @Test
+    void CheckJunit(){
+        assertEquals(2,2);
+    }
 }
